aa9525d303ecb31db243ecfa4e6425234631f78c
Cole Mathis <cole.mathis.ool@gmail.com>
2026-09-02 20:16:41 +0000

    Quiz from 08-31 and examples from variables and operators video

---
+++ 08-31/quiz.py
@@ -0,0 +1,14 @@
+# Anything that comes after a "#" is a comment
+# Python does not read/execute comments
+
+print("This is the first command of the file")
+# print("This line will not print")
+
+x = 10
+print("The value of x is:", x)
+
+# x = 11
+print("The value of x is:", x)
+
+x = 12
+print("The value of x is:", x)
---
+++ examples/variables-operators.py
@@ -0,0 +1,35 @@
+# int (Integers)
+my_variable = 5
+x = 11
+# print(my_variable)
+new_var = x % my_variable
+# print(new_var)
+another_var = x / my_variable
+
+
+# Floats
+
+# print(my_variable) # redefined the type
+my_variable = 5.0
+y = 1.0
+pi = 3.1415
+q = 41.5
+
+# print(pi ** my_variable)
+
+
+# Strings 
+name = "Cole"
+initial = "M"
+title = 'Ast Professor'
+
+combined = name
+# print(combined)
+
+# Bool
+is_raining = False
+is_monday = True
+
+
+diff_types = not (type(my_variable) == type(initial))
+print(diff_types)
