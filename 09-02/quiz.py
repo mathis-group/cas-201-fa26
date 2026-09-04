8a2c30cb5ab3b1cfee0b4ffaf1719e2a8130ecd9
Cole Mathis <cole.mathis.ool@gmail.com>
2026-09-04 17:40:46 +0000

    Quiz on bugs

---
+++ 09-02/quiz.py
@@ -0,0 +1,20 @@
+# A "bug" is a piece of code that produces an unexpected or undesired output
+# There's no such thing are 100% bug free code. 
+# Almost all scripts have problem that make for unexpected outputs.
+# Read this script carefully, it might not do what it says it does.
+
+# Define x and y to both be zero
+x = 0
+y = 0
+
+# print the values of x and y
+print("The value of x is:", x)
+print("The value of y is:", x)
+
+# update x and y
+x = 10
+y = 1
+
+# print the new values of x and y
+print("The value of x is:", x)
+print("The value of y is:", x)
